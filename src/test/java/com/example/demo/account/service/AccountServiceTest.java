56dd35210c1ea10217dee9a205698dd66140340c
huey-J <wkdgus7113@gmail.com>
2023-11-19 02:32:50 +0900

    (RED) AccountService 테스트 코드 추가

---
+++ src/test/java/com/example/demo/account/service/AccountServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.account.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+import com.example.demo.account.controller.AccountCreateRequestDTO;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountServiceTest {
+
+  @Mock
+  private AccountRepository accountRepository;
+  @InjectMocks
+  private AccountServiceImpl accountService;
+
+  @Nested
+  class create {
+
+    @Test
+    void 계좌_추가_성공_시_save메소드를_호출한다() {
+      // given
+      AccountCreateRequestDTO requestDTO = new AccountCreateRequestDTO(
+          "1111-15342-358923", "abc123", "홍길동", "홍길동의 계좌");
+      doNothing().when(accountRepository).save(any());
+
+      // when
+      accountService.create(requestDTO);
+
+      // then
+      verify(accountRepository, atLeastOnce()).save(any());
+    }
+  }
+
+}
