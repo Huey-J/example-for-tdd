a5c0cd9419721467f520fdbd778667725fc89727
huey-J <wkdgus7113@gmail.com>
2023-11-22 00:44:07 +0900

    (GREEN) AccountRepository existsByAccountNumber 구현

--- src/main/java/com/example/demo/account/repository/AccountRepository.java
+++ src/main/java/com/example/demo/account/repository/AccountRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.account.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
   boolean existsByAccountNumber(String accountNumber);
 
