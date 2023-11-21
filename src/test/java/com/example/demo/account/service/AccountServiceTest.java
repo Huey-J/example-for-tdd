3716194fb2fca2f59f8f5774c023f32bc0aee751
huey-J <wkdgus7113@gmail.com>
2023-11-22 00:00:34 +0900

    (RED) AccountService create 메소드 테스트 명세 추가

---
+++ src/test/java/com/example/demo/account/service/AccountServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.account.service;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class AccountServiceTest {
+
+  @Nested
+  class create {
+
+    @Test
+    void 요청한_계좌번호가_존재하지_않을_경우_새로_추가된_데이터를_반환한다() {
+    }
+
+    @Test
+    void 요청한_계좌번호가_이미_존재할_경우_예외를_발생시킨다() {
+    }
+
+  }
+
+}
